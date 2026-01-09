8912ef3b8d90ac44b965067e74067d7530b53922
catrpgorodroleplay-netizen <catrpgorodroleplay@gmail.com>
2026-01-09 19:26:22 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+// sw.js
+const CACHE_NAME = 'catrpgorod-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  // добавьте другие важные файлы
+];
+
+// Установка
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Активация и очистка старых кэшей
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
